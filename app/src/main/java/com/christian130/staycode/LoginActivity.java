a196410396341e548fece0a489816e864edd6aed
unknown <christian130@gmail.com>
2020-03-21 19:06:53 -0400

    what is in the kitchen

--- app/src/main/java/com/christian130/staycode/LoginActivity.java
+++ app/src/main/java/com/christian130/staycode/LoginActivity.java
@@ -3,6 +3,7 @@ package com.christian130.staycode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void devbutton(View v){
+
     }
 }
